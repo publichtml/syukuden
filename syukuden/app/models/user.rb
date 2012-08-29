4ee7856d311956cc89aa85720644c244e558017f
publichtml <publi9html@gmail.com>
2012-08-29 11:24:01 +0900

    save user_name and screen_name via twitter, facebook login

--- syukuden/app/models/user.rb
+++ syukuden/app/models/user.rb
@@ -5,23 +5,42 @@ class User < ActiveRecord::Base
   devise :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :provider, :uid
+  attr_accessible :provider, :uid, :user_name, :screen_name
 
   def self.find_or_create_by_twitter_auth(auth_info)
-  	find_or_create(auth_info['provider'], auth_info['uid'])
+  	find_or_create(
+      :provider => auth_info['provider'],
+      :uid => auth_info['uid'],
+      :user_name => auth_info['info']['nickname'],
+      :screen_name => auth_info['info']['name']
+    )
   end
 
   def self.find_or_create_by_facebook_auth(auth_info)
-  	find_or_create(auth_info['provider'], auth_info['uid'])
+    find_or_create(
+      :provider => auth_info['provider'],
+      :uid => auth_info['uid'],
+      :user_name => auth_info['info']['nickname'],
+      :screen_name => auth_info['info']['name']
+    )
   end
 
   private
 
-  def self.find_or_create(provider, uid)
-  	where(
-  		provider: provider,
-  		uid: uid
-  	).first_or_create
+  def self.find_or_create(user_info)
+    if user = where(:provider => user_info[:provider], :uid => user_info[:uid]).first
+      user.update_attributes!(
+        :user_name => user_info[:user_name], 
+        :screen_name => user_info[:screen_name]
+      ) unless user.user_name == user_info[:user_name] && user.screen_name == user_info[:screen_name]
+      user
+    else
+      create!(
+        :provider => user_info[:provider],
+        :uid => user_info[:uid],
+        :user_name => user_info[:user_name],
+        :screen_name => user_info[:screen_name]
+      )
+    end
   end
-
 end
